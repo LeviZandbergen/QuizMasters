d99e97a640e140c0bc4c62ef92b4c0232b703812
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2019-10-21 14:22:49 +0200

    added all files and finished Quiz1 H3

---
+++ Hoofdstuk3/Quiz1.java
@@ -0,0 +1,22 @@
+package Hoofdstuk3;
+
+public class Quiz1{
+	public static void main(String[] args) 
+	{
+		int i = 2;
+		
+		if(i == 2) 
+		{
+			System.out.print("N");
+		}
+		if(i > 0)
+		{
+			System.out.print("X");
+			System.out.print("Y");
+		}
+		if(i > 3) 
+		{
+			System.out.print("Z");
+		}
+	}
+}
