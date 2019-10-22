84f5a05a478c8254793fd6c0aa1088b0b84d6d37
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2019-10-22 10:38:30 +0200

    finished Quiz2

---
+++ Hoofdstuk4/Quiz2.java
@@ -0,0 +1,16 @@
+package Hoofdstuk4;
+
+public class Quiz2{
+	public static void main(String[] args)
+	{
+		int i = 8;
+		
+		while(i > 1)
+		{
+			i ++;
+			System.out.print(i);
+			i -= 5;
+		}
+	}
+}
+//Q1 'i' wont go down anymore so 'i' will never go under 1
