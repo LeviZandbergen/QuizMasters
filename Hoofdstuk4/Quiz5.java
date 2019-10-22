1e9530367039fef997925da06ce2e7179caaa9f5
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2019-10-22 10:59:33 +0200

    finished Quiz5

---
+++ Hoofdstuk4/Quiz5.java
@@ -0,0 +1,13 @@
+package Hoofdstuk4;
+
+public class Quiz5{
+	public static void main(String[] args)
+	{
+//		Changed the forloop
+		for(int i = 1 ; i < 10; i += 3)
+		{
+			System.out.print(i);
+		}
+	}
+}
+//Output 147
