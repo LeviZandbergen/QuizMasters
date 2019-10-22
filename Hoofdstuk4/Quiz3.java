46ad21fc22c11a748ccaa01be03e4e5317adae1f
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2019-10-22 10:47:38 +0200

    finished Quiz3/4

---
+++ Hoofdstuk4/Quiz3.java
@@ -0,0 +1,17 @@
+package Hoofdstuk4;
+
+public class Quiz3{
+	public static void main(String[] args)
+	{
+//		Changed value of var 'i' to 4
+		int i = 4;
+		do
+		{
+//			Changed value 5 to 6
+			i += 6;
+			System.out.print(i);
+		}
+		while(i <= 12);
+	}
+}
+//Q3 output is 1016
---
+++ Hoofdstuk4/Quiz4.java
@@ -0,0 +1,23 @@
+package Hoofdstuk4;
+
+public class Quiz4{
+	public static void main(String[] args)
+	{
+		int i = 2;
+		do
+		{
+			i += 3;
+			if(i != 4)
+			{
+				System.out.print("x");
+			}
+			else
+			{
+				System.out.print("y");
+			}
+		}
+		while(i < 10);
+	}
+}
+//Q1 Because 'i' will never be 10
+//Q2 XXX
