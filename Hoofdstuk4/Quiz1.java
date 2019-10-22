8abe28f0176e6a0938ca7a4fc76a55abcc019086
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2019-10-22 10:31:44 +0200

    added gitignore and finished Quiz1

---
+++ Hoofdstuk4/Quiz1.java
@@ -0,0 +1,18 @@
+package Hoofdstuk4;
+
+public class Quiz1{
+	public static void main(String[] args)
+	{
+		int i = 5;
+		
+		while(i > 1)
+		{
+			i --;
+			System.out.print(i);
+		}
+	}
+}
+//Q1 21
+//Q2 when 1 is used: i isn't bigger as 1 so it wont get into the loop
+//Q2 when 2 is used: 1
+//Q2 when 5 is used: 4321
