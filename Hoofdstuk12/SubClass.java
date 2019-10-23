4c61654a6b2b0c86284832f4608bee3e440ae098
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2019-10-23 16:35:54 +0200

    finished H12

---
+++ Hoofdstuk12/SubClass.java
@@ -0,0 +1,22 @@
+package Hoofdstuk12;
+
+public class SubClass{
+	
+	public static void main(String args[])
+	{
+//		New variables
+		int a = 35;
+		int b = 46;
+		double c = 46.98;
+		double d = 44.99;
+		double e = 81.0;
+		
+//		System output
+//		Gets highest number of both variables
+		System.out.print(Math.max(a, b) + "\n");
+//		Gets lowest number of both variables 
+		System.out.print(Math.min(c, d) + "\n");
+//		Gets square-root from variable
+		System.out.print(Math.sqrt(e) 	+ "\n");
+	}
+}
